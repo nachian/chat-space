f24a3bb663e9f9044f4ccc446d8926e1ac123407
nachian <natsuki.m0622@gmail.com>
2019-06-28 20:36:39 +0900

    group_creating_feature

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'messages#index'
   resources :users, only: [:edit, :update]
-
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
+ 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 end
+
