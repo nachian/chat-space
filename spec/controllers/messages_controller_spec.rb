f9bee26734b4f8a05143344697bfa504d709de2e
nachian <natsuki.m0622@gmail.com>
2019-07-06 17:42:52 +0900

    message,image save test

---
+++ spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe MessagesController, type: :controller do
+    let(:user){ create(:user) }
+    let(:group){ create(:group) }
+
+    describe 'GET#index' do
+        context 'can ' do
+
+        end
+        context 'can not ' do
+
+        end
+    end
+    
+end
