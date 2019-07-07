bd518b8ab1bcac15902c31e299cc45f858dfb3a2
nachian <natsuki.m0622@gmail.com>
2019-07-08 01:32:28 +0900

    complete

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -4,12 +4,34 @@ describe MessagesController, type: :controller do
     let(:user){ create(:user) }
     let(:group){ create(:group) }
 
-    describe 'GET#index' do
-        context 'can ' do
+    describe '#index' do
+        context 'log in' do
+            before do
+                login user
+                get :index, params: { group_id: group.id }
+            end
 
+            it 'assigns @message' do
+                expect(assigns(:message)).to be_a_new(Message)
+            end
+
+            it 'assigns @group' do
+                expect(assigns(:group)).to eq group
+            end
+
+            it 'render index' do
+                expect(response).to render_template :index
+            end
         end
-        context 'can not ' do
 
+        context ' not log in ' do
+            before do
+                get :index, params:{ group_id: group.id }
+            end
+
+            it 'redirects to new_user_session_path' do
+                expect(response).to redirect_to(new_user_session_path)
+            end
         end
     end
     
