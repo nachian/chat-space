d38b3064a5b2a84a2c6d6237de18f58cfb60f8f6
nachian <natsuki.m0622@gmail.com>
2019-07-18 22:02:45 +0900

    correction

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' 
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['keyDair']
+                  keys: ['~/.ssh/keyDair.pem']
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
